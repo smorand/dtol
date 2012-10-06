26c34a2efaa487e18e4b749a7cf09e86b4e98ce1
Sébastien Morand <seb.morand@gmail.com>
2012-10-06 17:11:10 +0000

    Remove old file

--- checkspawn.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ls src/public/images/spawns/ | while read a; do echo "select '${a/.png/}' as n, sum(c) from (select count(*) as c from dt_objects where name = '${a/.png/}' union select count(*) as c from dt_characters where name = '${a/.png/}') a;" | mysql -udtol -pdtol -Ddtol -N; done | grep 0 | grep -v '^fond-' | sed 's/ *0$//'
-

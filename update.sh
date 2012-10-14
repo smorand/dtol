1f49c52fec268dda2904480708483293cc6fcadb
Sébastien Morand <seb.morand@gmail.com>
2012-10-14 14:36:16 +0000

    Mise à jour du script de déploiement always data

--- update.sh
+++ update.sh
@@ -2,6 +2,5 @@
 
 pwd | grep -qE 'src$' && cd ..
 rsync -av --delete src/ dtol@ssh.alwaysdata.com:dt --exclude *.po --exclude *.pyc --exclude rooms
-echo "Set up new settings"
-ssh dtol@ssh.alwaysdata.com cp dt/scm/dtol/settings.py.alwaysdata dt/scm/dtol/settings.py
-
+#ssh app01 chgrp www-data dt -R
+ssh dtol@ssh.alwaysdata.com chmod go+w dt/resources/dtol.db
